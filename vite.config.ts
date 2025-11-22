5a5181555e623270c41e8794777ae6295d0fdfbd
savagrujic <savagrujic42@gmail.com>
2025-11-22 13:15:53 +0100

    Set base to root for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,22 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
+
   return {
-    base: '/StavaljskiSirSajt/', // ⬅️ ADD THIS LINE (exact repo name)
+    base: '/', // ⬅️ VAŽNO: za custom domain mora biti "/" !
 
     server: {
       port: 3000,
       host: '0.0.0.0',
     },
+
     plugins: [react()],
+
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
+
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
